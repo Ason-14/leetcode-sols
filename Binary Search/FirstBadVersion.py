49e5718e57afdc2734408fb8b45bf4a99a5c6eaf
Ason-14 <alanson.garcia21@gmail.com>
2026-05-14 12:57:05 +1000

    FirstBadVersion.py

--- Binary Search/FirstBadVersion.py
+++ Binary Search/FirstBadVersion.py
@@ -1,18 +1,24 @@
 # 278. First Bad Version
 
 # predefined isBadVersion API:
-# def isBadVersion(version: int) -> bool:
+def isBadVersion(version: int) -> bool:
+  return version >= bad
 
 class Solution:
   def firstBadVersion(self, n: int) -> int:
     R = n
     L = 1
-    M = L + ((L-R) // 2)
 
     while L < R:
+      M = L + ((R-L) // 2)
       if isBadVersion(M) is False:
         L = M + 1
       else:
         R = M
 
     return R
+
+sol = Solution()
+n = 10
+bad = 5
+test1 = print(sol.firstBadVersion(n))
