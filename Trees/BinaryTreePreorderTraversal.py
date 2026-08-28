061bcf1bee77eea29927e746fb5962269ec04511
Ason-14 <alanson.garcia21@gmail.com>
2026-08-28 19:30:49 +1000

    144. Binary Tree Preorder Traversal

---
+++ Trees/BinaryTreePreorderTraversal.py
@@ -0,0 +1,18 @@
+#144. Binary Tree Preorder Traversal
+
+from Trees.SameTree import TreeNode
+from typing import List, Optional
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def preOrder(node):
+            if not node:
+                return None
+            
+            result.append(node.val)
+            preOrder(node.left)
+            preOrder(node.right)
+        
+        preOrder(root)
+        return result
