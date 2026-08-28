14d8e7234b411e80a4d8c6a90f695f74c5456599
Ason-14 <alanson.garcia21@gmail.com>
2026-08-28 19:32:44 +1000

    #145. Binary Tree Postorder Traversal

---
+++ Trees/BinaryTreePostorderTraversal.py
@@ -0,0 +1,18 @@
+#145. Binary Tree Postorder Traversal
+
+from Trees.SameTree import TreeNode
+from typing import List, Optional
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def postOrder(node):
+            if not node:
+                return
+            postOrder(node.left)
+            postOrder(node.right)
+            result.append(node.val)
+        
+        postOrder(root)
+        return result
