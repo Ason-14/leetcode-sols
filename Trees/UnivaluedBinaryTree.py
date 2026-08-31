4eef2ce0b5b8b6f7a5a5fafe2f840ad40f9b082b
Ason-14 <alanson.garcia21@gmail.com>
2026-08-31 22:02:01 +1000

    965. Univalued Binary Tree

---
+++ Trees/UnivaluedBinaryTree.py
@@ -0,0 +1,18 @@
+#965. Univalued Binary Tree
+
+from Trees.SameTree import TreeNode
+from typing import Optional
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        num = root.val
+
+        def unival(node, target):
+            if not node:
+                return
+            elif node.val == target:
+                return True
+            else:
+                return False
+            return unival(node.left, target) and unival(node.right, target)
+        
+        return unival(root, num)
